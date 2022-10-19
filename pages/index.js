7edb1df99c632388ffa028407ec22abe18f4eca1
newton mathias <newtymathias@gmail.com>
2022-10-19 18:54:57 +0300

    services

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Services from '../components/Services'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+      <Services />
     </div>
   )
 }
