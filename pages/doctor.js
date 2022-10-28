4fb68cdfbd7c6b4c3ad64b7cb808d3b282bfb1bb
newton mathias <newtymathias@gmail.com>
2022-10-28 17:13:57 +0300

    Update doctor.js

--- pages/doctor.js
+++ pages/doctor.js
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Image from 'next/image';
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 import { 
     StarIcon,
