74d13557fd2cd03350f1ef3566498b2e488a09eb
newton mathias <newtymathias@gmail.com>
2022-10-28 16:12:28 +0300

    ver

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import Services from '../components/Services'
 import TeleFeed from '../components/TeleFeed'
 
 export default function Home({ doctorsList }) {
+
+
   return (
     <div>
       <Head>
@@ -28,7 +30,13 @@ export default function Home({ doctorsList }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/doctors");
+  let dev = process.env.NODE_ENV !== 'production';
+    let { DEV_URL, PROD_URL } = process.env;
+
+    // request posts from api
+    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/doctors`);
+    // extract the data
+    let data = await response.json();
   return {
     props: {
       doctorsList: res.data,
