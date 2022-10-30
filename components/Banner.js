b30cfd40e6b13ee27061a2711f21592b798328e4
newton mathias <newtymathias@gmail.com>
2022-10-30 19:09:44 +0300

    responsive

--- components/Banner.js
+++ components/Banner.js
@@ -2,34 +2,31 @@ import Image from 'next/image'
 
 function Banner() {
   return (
-    <div className='bg-color1 flex h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[500px] 2xl:h-[500px]'>
-      {/*right side */}
-      <div className='basis-1/2 m-auto'>
-        <div className='bg-white p-10 w-[600px] m-auto -mr-10'>
-          <p className='text-5xl font-bold'>Not just a doctor</p>
-          <p className='text-indigo-500 text-5xl font-bold'>your doctor</p>
-          <p className='py-4'>
-            Find the best price for the highest quality physicians. Get connected to a doctor in minutes.
-          </p>
-          <button className='button'>
-            Get started
-          </button>
-          <p className='py-4'>Named Best Overall Telehealth by MOH</p>
-        </div>
-        
-      </div>
-
-      {/*left side */}
-      <div className='basis-1/2 relative '>
+    <div className='flex h-[700px] bg-color1'>
+      <div className=' flex items-center justify-center md:basis-1/2 md:ml-auto relative w-full mb-60 md:mb-1'>
           <Image
           alt='banner'
           src="/kowalievska.jpg"
           layout='fill'
           objectFit='cover'
-  />
+          />
       </div>
+      <div className='absolute left-0 right-0 m-auto bottom-1 w-3/4  md:basis-1/2 md:w-[600px] md:top-1/4 md:right-1/3'>
+            <div className='p-6  bg-white opacity-80 text-center md:p-10'>
+              <p className=' font-serif font-extrabold text-2xl md:text-5xl '>Not just a doctor</p>
+              <p className='text-indigo-800 text-2xl font-serif font-extrabold md:text-5xl'>your doctor</p>
+              <p className='py-4 font-serif'>
+                Find the best price for the highest quality physicians. Get connected to a doctor in minutes.
+              </p>
+              <button className=' time-button bg-indigo-500 md:button'>
+                Get started
+              </button>
+              <p className='py-4'>Named Best Overall Telehealth by MOH</p>
+            </div>
+          </div>
     </div>
   )
 }
+
 
 export default Banner
--- components/Header.js
+++ components/Header.js
@@ -1,39 +1,66 @@
 import { 
+    Bars3Icon,
     MagnifyingGlassIcon,
     MapPinIcon,
-    UserIcon
+    UserIcon,
+    XMarkIcon
  } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
     const router = useRouter();
- return (
-    <div className="sticky bg-white shadow-md top-0 z-50 flex p-4">
-        {/*left Nav*/}
-        <div className="flex m-2 items-center flex-grow space-x-5">
-            <p onClick={ () => router.push('/')} className="p-1 text-indigo-500 text-3xl font-bold cursor-pointer">MyHealth</p> 
-            <p className="p-1 font-bold">Patients</p>
-            <p className="p-1 font-bold">Providers</p>
-        </div>
-        {/*right Nav */}
-        <div className="flex justify-end space-x-6">
-            <div className="flex relative  items-center cursor-pointer text-indigo-500">
-                <input type="text" placeholder="Doctor name, Service, Symptom" className="pl-8 w-80 items-center focus:outline-none border-b border-indigo-500"/>
-                <MagnifyingGlassIcon className="h-14 p-4  absolute inset-y-0 left-0 flex items-center pl-0" />
-            </div>
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-            <div className="flex relative items-center  cursor-pointer text-indigo-500">
-                <input type="text" className="pl-8 items-center focus:outline-none  border-b border-indigo-500" placeholder="Westlands,Nairobi"/>
-                <MapPinIcon className="h-14 p-4 absolute inset-y-0 left-0 flex items-center pl-0"/>
-            </div>
-            
-            <div className="flex items-center text-indigo-500">
-                <UserIcon className="h-3/4 p-1"/>
-                <p className="mr-2">Login</p>
+ return (
+    <nav className="sticky bg-white shadow top-0 z-50  p-4">
+        <div className="mx-auto px-4">
+            <div className="flex justify-between relative">
+                <div className="flex space-x-7">
+                    {/*left Nav*/}
+                    <div className="text-indigo-800 hover:text-indigo-500 transition duration-300 text-3xl font-bold cursor-pointer m-auto">
+                        <p onClick={ () => router.push('/')} className="">MyHealth</p>   
+                    </div>
+                    <div className="hidden md:flex items-center space-x-1">
+                        <a className="py-4 px-2 text-indigo-800 font-semibold hover:text-indigo-500 transition duration-300">Patients</a>
+                        <a className="py-4 px-2 text-indigo-800 font-semibold hover:text-indigo-500 transition duration-300">Providers</a>
+                    </div>
+                    {/*right Nav */}
+                </div>
+                <div className="hidden md:flex justify-end space-x-6">
+                    <div className="flex relative  items-center cursor-pointer text-indigo-800 font-semibold hover:text-indigo-500 transition duration-300">
+                        <input type="text" placeholder="Doctor, Service, Symptom" className="pl-8  items-center focus:outline-none border-b border-indigo-500"/>
+                        <MagnifyingGlassIcon className="h-14 p-4  absolute inset-y-0 left-0 flex items-center pl-0" />
+                    </div>
+                    <div className="flex relative items-center  cursor-pointer text-indigo-800 font-semibold hover:text-indigo-500 transition duration-300">
+                        <input type="text" className="pl-8 items-center focus:outline-none  border-b border-indigo-500" placeholder="Westlands,Nairobi"/>
+                        <MapPinIcon className="h-14 p-4 absolute inset-y-0 left-0 flex items-center pl-0"/>
+                    </div>
+                    
+                    <div className="flex items-center text-indigo-800 font-semibold hover:text-indigo-500 transition duration-300">
+                        <UserIcon className="h-3/4 p-1"/>
+                        <p className="mr-2">Login</p>
+                    </div>
+                </div>
+                {/*mobile Icon bars */}
+                <div className="md:hidden flex items-center">
+						<Bars3Icon className={isNavExpanded ? "hidden" : "h-6 w-6"} onClick={() => {setIsNavExpanded(!isNavExpanded)}}/>
+				</div>
+                <div className={isNavExpanded ? "absolute p-5 m-auto bg-white" : "hidden"}>
+                    <div className="">
+                        <XMarkIcon className="h-6 w-6" onClick={() => {setIsNavExpanded(!isNavExpanded)}}/>
+                        <p>Login</p>
+                        <p>Sign Up</p>
+                        <p>Televisit</p>
+                        <p>Book Appointment</p>
+                        <p>Provider Login</p>
+                    </div>
+			    </div>
+                
             </div>
         </div>
-    </div>
+
+    </nav>
   )
 }
 
