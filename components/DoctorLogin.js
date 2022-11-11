b0d9dfe12d09eb85af1219f2113dee14d93b2608
newton mathias <newtymathias@gmail.com>
2022-11-11 13:05:23 +0300

    Update DoctorLogin.js

--- components/DoctorLogin.js
+++ components/DoctorLogin.js
@@ -85,7 +85,7 @@ const submitHandler = async (e) => {
                     <a className="text-gray-500" href="#!">Forgot password?</a>
                   </div>
                   <div className="flex items-center justify-between pb-6">
-                    <p className="mb-0 mr-2">Don't have an account?</p>
+                    <p className="mb-0 mr-2">Don&apos;t have an account?</p>
                     <Link href='/doctor/register'>
                         <button
                             type="button"
