ab6571736e19742269fc1bea76200491d3f95291
newton mathias <newtymathias@gmail.com>
2022-11-22 09:38:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ module.exports = {
 
     STRIPE_API_KEY: 'pk_test_51LtCmJHdzKEQrGTDRMQtaerYiffZfSZlUi1fVizOW2XAnFRuKAbRutfR30r3AxGyrzWMe5OnKNP9PEnn8clyKWsd007Cv4XKdX',
     STRIPE_SECRET_KEY: 'sk_test_51LtCmJHdzKEQrGTDSIPvAvbqztroR7nruZISYKJkc9eTh37Sdqj0XBjgN1djywblFnU8WKKsulai5Okhven7Fs9Z00TV9sSAcn',
-    STRIPE_WEBHOOK_SECRET: 'whsec_7048bbd4c066137f36d16141f485f7135b767272b8cb0373dad456e7d4095fac'
+    STRIPE_WEBHOOK_SECRET: 'whsec_8DsdgkqX7a3hrA8I6TTmE4PodBbimdRb',
+    NEXTAUTH_URL :'ehealthcare-murex.vercel.app'
 
 
 },
